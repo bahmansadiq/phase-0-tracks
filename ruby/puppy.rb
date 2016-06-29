022411acdc6464228b6d1cd6e0099de5983e4b8a
bahmansadiq <bahman.sadiq@gmail.com>
2016-06-28 18:40:13 -0700

    Update puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+
+#characteristics:
+
+#number of legs: 4
+#number of eyes: 2
+#frindly : yes
+# Does it eat vegetables : no
+#Can be adopted as a pet: yes
+#color varies: yes
+#Minimum weight: 2 lbs
+#Maximum Weight : 20 lbs
+# does it have nails: yes
+#Exterior Hears:yes
+
+
+#Behavior:
+#Jump:yes
+#Run: yes
+#bite:yes
+#Scratch:yes
+#carry weight:no .
