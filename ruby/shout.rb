2e22cb00449eaa89b26e5f542a7ecd4f05db83c1
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-07-01 00:57:42 -0700

    Add mixin module and include it  inside the classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,10 +1,27 @@
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
     words + "!!!" + " :("
+    
   end
-   def self.yelling_happy(name)
+   def yelling_happy(name)
+   	
    	p name 
    end
 end
 
-Shout.yelling_happy("Yelling Happy")
+#Shout.yelling_happy("Yelling Happy")
+
+class Noise_poluion
+	include Shout
+end
+
+class Yelling_sound
+	include Shout
+end
+
+
+noise=Noise_poluion.new
+p noise.yell_angrily("yelling really angrily!")
+
+sound=Yelling_sound.new 
+sound.yelling_happy("yelling sounds are annoying")
