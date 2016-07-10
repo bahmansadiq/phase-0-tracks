34a5e65e3cecfedc31a06c88a4c4e32f9e760c95
Kari Anne <kariag@icloud.com>
2016-07-10 10:32:12 -0500

    iterate through items on list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,87 +1,73 @@
-#pgs2_2
+#gps2_2
 
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
+
 # steps:
-# split up the string
-# Add the user input string to the Hash
-
-
-# set default quantity
-# Set the item number
-
-# print the list to the console [can you use one of your other methods here?]
-# store the Hash inside another variable and print the result variable by the end of program
-# output: [what data type goes here, array or hash?]
-# output the hash we created to the user
-
+  # split items into an array
+  # access items individually
+  # iterate through items and push to hash container along with assigning
+  # default quantity for items
+  # print the list to the console [can you use one of your other methods here?]
+# output: output would be a hash
 
 # Method to add an item to a list
 # input: item name and optional quantity
-# steps: Ask user to input data, change the input to array, call the list_method and pass the parameters to it.
-# output:
+# steps: create hash with assigned key and value
+  # push new hash to primary hash container
+# output: new hash key and value
 
 # Method to remove an item from the list
-# input: remove an item name and quantity
-# steps:go inside the list_method, hash key and value remove the selected item
-# output: The update list in list_method
+# input: primary hash container and item name key
+# steps: remove item by accessing the appropriate hash key
+# output: printed primary hash container items with appropriate item removed
 
 # Method to update the quantity of an item
-# input:update the quantit of an item
-# steps: create a loop to ask for more inputs
-# output: call the list_method to print the list
+# input: name of hash container and appropriate key
+# steps: access primary hash container and the appropriate key
+  # set key value to new value
+# output: hash key with new value
 
 # Method to print a list and make it look pretty
-# input: list of items
-# steps: get the data from user, Go through the methods
-# output: the list
+# input: items
+# steps: iterate through items with their key and values formatted to #specific layout
+# output: printed format of key and value pairs
+
+items = 'carrots apples cereal pizza'
+empty_hash = {}
 
 
-def new_list(items)
-  list = {}
-  split_items = items.split(' ')
-  split_items.each { |item|
-    list[item] = 1
-  }
-  print_list(list)
-  list
-end
-
-def add_item(list, item, quantity = 1)
-  list[item] = quantity
-  list
-end
-
-def remove_item(list, item)
-  list.delete(item)
-  list
-end
-
-def update_qty(list, item, quantity)
-
-  if list[item] != nil
-    list[item] = quantity
+def create_list(items)
+  each_item = {}
+  food = items.split(' ')
+  food.each do |item|
+    each_item[item] = 0
   end
-  list
+  each_item
 end
 
-def print_list(list)
-  puts "Current List:"
-  if list.length != 0
-    list.each { |item, quantity|
-      puts "#{item}\t#{quantity}"
-    }
-  else
-    puts "BLANK"
+def add_item(item)
+  create_list(item)
+end
+
+def remove_item(item)
+  item.delete('carrots')
+end
+
+def update_value(value)
+  value['cereal'] = 2
+end
+
+def looking_good(empty_hash)
+  puts "Our grocery list"
+  puts "-" * 20
+  empty_hash.each do |key, value|
+    puts "Please buy: #{value} #{key}"
   end
 end
 
-
-testList = new_list("")
-testList = add_item(testList, "Lemonade", 2)
-testList = add_item(testList, "Tomatoes", 3)
-testList = add_item(testList, "Onions", 1)
-testList = add_item(testList, "Ice Cream", 4)
-testList = remove_item(testList, "Lemonade")
-testList = update_qty(testList, "Ice Cream", 1)
-print_list(testList)
+p empty_hash.merge!add_item('bananas')
+p empty_hash.merge!(create_list(items))
+p remove_item(empty_hash)
+p update_value(empty_hash)
+looking_good(empty_hash)
