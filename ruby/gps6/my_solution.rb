6af0b22e9a071251e1410205146ef2fec745ab30
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-07-14 23:20:00 -0700

    Add comment reflection for the given terms

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -95,3 +95,24 @@ STATE_DATA.each do |state, data|
   state_name = VirusPredictor.new(state, data[:population_density], data[:population])
   state_name.virus_effects
 end
+
+
+#release 9
+
+#What are the differences between the two different hash syntaxes shown in the state_data file?
+# hash is created by using its implicit form and Each named key is a symbol 
+
+#What does require_relative do? How is it different from require?
+#Require_relative is used to load a file and its shortcut form of require, where in require_relative you don't need to give full_Path of the file when 
+#its in same directory
+
+#What are some ways to iterate through a hash?
+# Using block to go through each hash key and value using (each do ...code... end ) and also can be used manually hash_name[:key]=value.
+
+
+#When refactoring virus_effects, what stood out to you about the variables, if anything?
+# That they are instance variables and can be called any where within the class, so i have called (@state) directly and two other variables with the methods.
+
+#What concept did you most solidify in this challenge?
+# I can say iteration through hash in hash.
+
