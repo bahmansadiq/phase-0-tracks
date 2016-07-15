568d9a75d1d94ebb3e678e40a98e21c5c6cd95c8
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-07-15 00:41:16 -0700

    Add condition before the user output print

--- js/explore.js
+++ js/explore.js
@@ -6,13 +6,20 @@
 // return the joined seris of character in the array 
 
 function reverse(word) {
-	reverse_array=[];
-	var len=word.length;
-	for( var i=0; i<=len; i++)
-	reverse_array.push(word.charAt(len - i));
-	return reverse_array.join('')
-	
-
-}
-
+			reverse_array=[];
+			var len=word.length;
+			for( var i=0; i<=len; i++)
+			
+			reverse_array.push(word.charAt(len - i));
+			if (word!="Obama"){
+		 
+			return reverse_array.join('')
+			}
+		    else
+		   console.log("Can't reverse name of the president:")
+		    return word;
+		    end 
+  }
 console.log(reverse("bahman"))
+
+console.log(reverse("Obama"))
