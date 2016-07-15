a6094642983a4472c8106f5e4290e2e0ce6fc02d
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-07-15 00:21:19 -0700

    Add psuedocode to break down the task to plain language

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+//define a function which takes one argument
+// access the parameter
+// find length of the parameter to set on condition
+// introduce an empty array
+// access each character of the given parameter split them and push it to the array
+// return the joined seris of character in the array 
+
