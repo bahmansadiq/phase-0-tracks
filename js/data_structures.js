ac86fbc7ca5699646c964dd902566ac8ed24fd6e
gem install rails -v 4.2.3 <bahman.sadiq@gmail.com>
2016-07-16 13:11:14 -0700

    Add constructor with dif datatypes and function

--- js/data_structures.js
+++ js/data_structures.js
@@ -18,3 +18,35 @@ return Horse
 }
 newHorse(horseNames, Colors)
 
+//out put:
+// { Ed: 'blue', Mac: 'green', Falstad: 'red', Jerry: 'yellow' }
+
+// Release 2:
+
+function car(model, year, speed) {
+	this.model = model;
+	this.year = year;
+	this.speed = speed;
+	this.braketime = function() {
+		if (this.speed > 200){
+			console.log("The breaktime is not strong enough for " + this.speed + "mph!!");
+		}
+		 else 
+			 {
+				console.log("all good under the hood!!");
+			 }
+		};
+} 
+
+
+
+
+
+anothercar = new car("Toyota", 2016, 209)
+morecars= new car("Honda", 2010, 100)
+anothercar.braketime()
+morecars.braketime()
+
+// outputs: 
+// The breaktime is not strong enough for 209mph!!
+//all good under the hood!!
